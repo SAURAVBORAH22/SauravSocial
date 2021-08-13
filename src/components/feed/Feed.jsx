179bb562a1bde199af0cece1d54222fe6ba2f6f3
SAURAVBORAH22 <sauravborah1999@gmail.com>
2021-08-14 01:05:13 +0530

    Feed , share ,post components

--- src/components/feed/Feed.jsx
+++ src/components/feed/Feed.jsx
@@ -1,11 +1,26 @@
 //importing the feed.css
 import './feed.css';
+//importing Share.jsx
+import Share from '../share/Share';
+//importing the post.jsx
+import Post from '../post/Post';
 
 //creating the feed component
 export default function Feed() {
     return (
         <div className="feed">
-            Feed
+            {/* wrapping up the feed */}
+            <div className="feedWrapper">
+                <Share />
+                <Post />
+                <Post />
+                <Post />
+                <Post />
+                <Post />
+                <Post />
+                <Post />
+                <Post />
+            </div>
         </div>
     )
 }
