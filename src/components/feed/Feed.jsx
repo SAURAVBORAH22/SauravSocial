bc13d17b4e7f2228c8d555d8047444b993f03a18
SAURAVBORAH22 <sauravborah1999@gmail.com>
2021-08-15 20:50:57 +0530

    add dummy data

--- src/components/feed/Feed.jsx
+++ src/components/feed/Feed.jsx
@@ -4,6 +4,8 @@ import './feed.css';
 import Share from '../share/Share';
 //importing the post.jsx
 import Post from '../post/Post';
+//importing Posts from dummyData
+import { Posts } from "../../dummyData";
 
 //creating the feed component
 export default function Feed() {
@@ -11,15 +13,11 @@ export default function Feed() {
         <div className="feed">
             {/* wrapping up the feed */}
             <div className="feedWrapper">
+                {/* showing the share component */}
                 <Share />
-                <Post />
-                <Post />
-                <Post />
-                <Post />
-                <Post />
-                <Post />
-                <Post />
-                <Post />
+                {Posts.map(p => { //mapping the posts
+                    <Post post={p}/>//returning the post
+                })}
             </div>
         </div>
     )
